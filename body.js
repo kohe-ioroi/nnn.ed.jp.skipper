c4647c713a2f80c1b0c00b14e37c8a872eb65312
五百藏康平 <>
2018-04-17 12:33:45 +0900

    新バージョンに対応

--- body.js
+++ body.js
@@ -1,55 +1,41 @@
-function onError(error) {}
-
-function onGot(item) {
-  var skip = "True";
-  var autostart = "True";
-  var playedalert = "True";
-  if (item.skip) {
-    var skip = item.skip;
-  }
-  if (item.autostart) {
-    var autostart = item.autostart;
-  }
-  if (item.playedalert) {
-    var playedalert = item.playedalert;
-  }
-  if (skip != "True" && autostart != "True" && playedalert != "True") {} else {
-    document.body.style.border = "5px solid red";
-  }
-  if (!document.evaluate('//*[@id="nextMovie"]', document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null).snapshotLength) {
-    if (!document.evaluate('//*[@id="nextTest"]', document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null).snapshotLength) {
-      if (playedalert == "True") {
-        alert("レポート・設問の回答を行ってください。");
-      } else {};
-    } else {
-      try {
-        if (autostart == "True") {
-          setTimeout(function () {
-            document.getElementsByClassName('play_state_wait')[0].click();
-          }, 1000);
-        } else {};
-      } catch (e) {};
-      if (skip == "True") {
-        setInterval(function () {
-          document.getElementById('nextTest').click();
-        }, 1000);
-      } else {
-
-      };
-    }
-  } else {
-    try {
-      setTimeout(function () {
-        document.getElementsByClassName('play_state_wait')[0].click();
-      }, 1000);
-    } catch (e) {};
-    if (skip == "True") {
-      setInterval(function () {
-        document.getElementById('nextMovie').click();
-      }, 1000);
-    } else {};
-  }
-
+function getElementByXpath(path) {
+    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
-var getting = browser.storage.local.get();
-getting.then(onGot, onError);
+function getNowVideoId() {
+    var Ele = getElementByXpath('/html/body/div[1]/div[3]/div[2]/div/div[2]/div[1]/div[1]/ul');
+    var l = Ele.childNodes.length;
+
+    for (var i = 1; i < l; i++) {
+        var _className = getElementByXpath('/html/body/div[1]/div[3]/div[2]/div/div[2]/div[1]/div[1]/ul/li[' + i + ']/a').className;
+        if (_className == "is-selected") {
+            var videoid = i;
+            break;
+        } else {
+        }
+    }
+    return videoid;
+}
+function nextEvent() {
+    getElementByXpath('/html/body/div[1]/div[3]/div[2]/div/div[2]/div[1]/div[1]/ul/li[' + (getNowVideoId() + 1) + ']/a').click();
+}
+function eventInfo(id) {
+    var _temp = getElementByXpath('/html/body/div[1]/div[3]/div[2]/div/div[2]/div[1]/div[1]/ul/li[' + id + ']/a').offsetParent.className;
+    if (_temp == "movie") {
+        return "Movie";
+    } else if (_temp == "evaluation-test") {
+        return "EvalTest";
+    } else if (_temp == "movie good") {
+        return "Finish";
+    } else if (_temp == "essay-test") {
+        return "EssayTest";
+    } else {
+        return False;
+    }
+}
+setInterval(function () {
+    if (eventInfo(getNowVideoId()) == "Video" || eventInfo(getNowVideoId()) == "Finish") {
+            nextEvent();
+    } else if (eventInfo(getNowVideoId()) == "EvalTest" || eventInfo(getNowVideoId()) == "EssayTest") {
+        
+    } else { }
+}, 5000);
